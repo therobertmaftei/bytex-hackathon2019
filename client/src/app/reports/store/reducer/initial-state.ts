f17db69c70c79116d31485c693b5cc89060d53c9
Robert Maftei <mafrobert97@gmail.com>
2019-10-06 06:16:06 +0300

    Report done

--- client/src/app/reports/store/reducer/initial-state.ts
+++ client/src/app/reports/store/reducer/initial-state.ts
@@ -3,12 +3,19 @@ import { fromJS, List } from 'immutable';
 import { IReport } from '../../models/report.model';
 
 export interface IState {
+  report: IRequest<IReport>;
   reports: IRequest<List<IReport>>;
   location: Partial<Coordinates>;
 }
 
 export const initialState: IState = {
   reports: fromJS({
+    message: [],
+    data: null,
+    success: false,
+    loading: false
+  }),
+  report: fromJS({
     message: [],
     data: null,
     success: false,
--- client/src/app/shared/services/http.service.ts
+++ client/src/app/shared/services/http.service.ts
@@ -11,13 +11,13 @@ export class HTTPService {
   private server: string = `${environment.server.ip}`;
   private headers: HttpHeaders = new HttpHeaders({
     // tslint:disable-next-line: max-line-length
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlb2Rvci5wcm9jYUBieXRleC5ybyIsImZpcnN0bmFtZSI6IlRlb2RvciIsImxhc3RuYW1lIjoiUHJvY2EiLCJwcm9maWxlUGljdHVyZSI6Imh0dHBzOi8vZGV2LWFwaS1oZWNrLnMzLmV1LWNlbnRyYWwtMS5hbWF6b25hd3MuY29tL3Byb2ZpbGVQaWN0dXJlcy8xNzlhZjVhOC0xYWRkLTRjYjEtYWM5ZS0yYTMwOTFkNmRkZjAucG5nIiwiX192IjowLCJhdWQiOiJodHRwczovL2FwaS5kZXYuaGVjay5ybyIsImlzcyI6ImFwaS5kZXYuaGVjay5ybyIsInVzZXI6aWQiOiI1ZDk4NmM1ZTNmM2M3NzljNWU4NDVkYjUiLCJpYXQiOjE1NzAyNzAzMDIsImV4cCI6MTU3MDg3NTEwMn0.pYJys6cJl_eKejVz3C3JI-c622VUMl0teaAZZIWdgR4'
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjcmVhdGVkQXQiOiIyMDE5LTEwLTA2VDAxOjQ2OjQyLjQwOFoiLCJ1cGRhdGVkQXQiOiIyMDE5LTEwLTA2VDAxOjQ2OjQyLjQwOFoiLCJlbWFpbCI6Imdlb3JnZS5tb3NjdUBieXRleC5ybyIsImZpcnN0bmFtZSI6Ikdlb3JnZSIsImxhc3RuYW1lIjoiTW9zY3UiLCJwcm9maWxlUGljdHVyZSI6Imh0dHBzOi8vZGV2LWFwaS1oZWNrLnMzLmV1LWNlbnRyYWwtMS5hbWF6b25hd3MuY29tL3Byb2ZpbGVQaWN0dXJlcy80NGYwNTdhYS1mZmRlLTRmZWMtODA3YS1lYmNjYTA2NzU2YjkucG5nIiwiX192IjowLCJhdWQiOiJodHRwczovL2FwaS5kZXYuaGVjay5ybyIsImlzcyI6ImFwaS5kZXYuaGVjay5ybyIsInVzZXI6aWQiOiI1ZDk5NDc4MjIzZjBlYWE4MTQ0ZjZiMGIiLCJpYXQiOjE1NzAzMjkwMDEsImV4cCI6MTU3MDkzMzgwMX0.4cwwsWrabbAKexY7S0QqLhu7hgco5Lk2Sfwafm44hXI'
   });
 
   constructor(protected http: HttpClient) { }
 
   public post<P, T>(endpoint: string, body: P, queryParams?: IQueryParam): Observable<IRequest<T>> {
-    return this.http.post<IRequest<T>>(this.buildEndpoint(endpoint, queryParams), body);
+    return this.http.post<IRequest<T>>(this.buildEndpoint(endpoint, queryParams), body, { headers: this.headers });
   }
 
   public get<T>(endpoint: string, queryParams?: IQueryParam): Observable<IRequest<T>> {
@@ -25,11 +25,11 @@ export class HTTPService {
   }
 
   public put<P, T>(endpoint: string, body: P, queryParams?: IQueryParam): Observable<IRequest<T>> {
-    return this.http.put<IRequest<T>>(this.buildEndpoint(endpoint, queryParams), body);
+    return this.http.put<IRequest<T>>(this.buildEndpoint(endpoint, queryParams), body, { headers: this.headers });
   }
 
   public delete<T>(endpoint: string, queryParams?: IQueryParam): Observable<IRequest<T>> {
-    return this.http.delete<IRequest<T>>(this.buildEndpoint(endpoint, queryParams));
+    return this.http.delete<IRequest<T>>(this.buildEndpoint(endpoint, queryParams), { headers: this.headers });
   }
 
   private buildEndpoint(endpoint: string, queryParams?: IQueryParam): string {
